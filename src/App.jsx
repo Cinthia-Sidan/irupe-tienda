8e770749dd7ebd89f6091c4ad05a91001350e5cf
Cinthia Sidan <sidanusp@gmail.com>
2023-06-09 18:42:25 -0300

    Cree la app en react, el navbar y el mensaje.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <NavBar/>
-      <ItemListContainer greeting = {"Página de prueba"}/>
+      <ItemListContainer greeting = {"Página de prueba!!"}/>
     </>
   )
 }
